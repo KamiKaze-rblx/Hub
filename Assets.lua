3010ca747b4d6b38e80326a9309ce98447b3ad0e
KamiKaze-rblx <rblxkamikaze@gmail.com>
2025-04-02 22:00:36 +0300

    Update Assets.lua

--- Assets.lua
+++ Assets.lua
@@ -1,16 +1,52 @@
 local v0 = {
-	Gems1 = "<:BlueGemPile:1357014156422811861>",
-	Gems2 = "<:BagofBlueGems:1357014152844935229>",
-	Gems3 = "<:BagofBlueGems:1357014152844935229>",
-	Ticket1 = "<:Ticket1:1357014159434453124>",
-	Ticket2 = ">:Ticket2:1357014160956981340>",
-	Ticket3 = "<:TicketBag:1357014162999611442>",
-	Beru = "<:Beru1:1357014154585702570>",
-	CrystalSword = "<:CrystalSword:1357014157810995400>",
-	Tiger = "<:Tiger:1357014164605767770>",
-	Dust1 = "<:COMMON_POWDER:1357008889220173964>",
-	Dust2 = "<:RareDust:1357011491223507164>",
-	Dust3 = "<:LEGENDARYPOWDER:1357010255586001098>"
+	Gems1 = {
+		Name = "+100 Gems",
+		Image = "<:BlueGemPile:1357014156422811861>"
+	},
+	Gems2 = {
+		Name = "+500 Gems",
+		Image = "<:BagofBlueGems:1357014152844935229>"
+	},
+	Gems3 = {
+		Name = "+2000 Gems",
+		Image = "<:BagofBlueGems:1357014152844935229>"
+	},
+	Ticket1 = {
+		Name = "+2 Tickets",
+		Image = "<:Ticket1:1357014159434453124>"
+	},
+	Ticket2 = {
+		Name = "+5 Tickets",
+		Image = ">:Ticket2:1357014160956981340>"
+	},
+	Ticket3 = {
+		Name = "+8 Tickets",
+		Image = "<:TicketBag:1357014162999611442>"
+	},
+	Beru = {
+		Name = "+1 Ziru G",
+		Image = "<:Beru1:1357014154585702570>"
+	},
+	CrystalSword = {
+		Name = "+1 Crystal Sword",
+		Image = "<:CrystalSword:1357014157810995400>"
+	},
+	Tiger = {
+		Name = "+1 Tiger",
+		Image = "<:Tiger:1357014164605767770>"
+	},
+	Dust1 = {
+		Name = "+20 Common Powder",
+		Image = "<:COMMON_POWDER:1357008889220173964>"
+	},
+	Dust2 = {
+		Name = "+20 Rare Powder",
+		Image = "<:RareDust:1357011491223507164>"
+	},
+	Dust3 = {
+		Name = "+20 Legendary Powder",
+		Image = "<:LEGENDARYPOWDER:1357010255586001098>"
+	}
 };
 
 return v0;
