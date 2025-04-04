94405f32f884521bbd692e732634e2c390420c4d
KamiKaze-rblx <rblxkamikaze@gmail.com>
2025-04-04 17:20:50 +0300

    Update Dungeon.lua

--- Dungeon.lua
+++ Dungeon.lua
@@ -1,29 +1,29 @@
 local v0 = {
-	LevelingCity = {
+	"<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Leveling City!</font>" = {
 		Name = "Leveling City",
 		Text = "<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Leveling City!</font>"
 	},
-	[2] = {
+	"<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Grass Village!</font>" = {
 		Name = "Grass Village",
 		Text = "<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Grass Village!</font>"
 	},
-	[3] = {
+	"<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Brum Island!</font>" = {
 		Name = "Brum Island",
 		Text = "<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Brum Island!</font>"
 	},
-	5 = {
+	"<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Faceheal Town!</font>" = {
 		Name = "Faceheal Town",
 		Text = "<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Faceheal Town!</font>"
 	},
-	6 = {
+	"<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Lucky Kingdom!</font>" = {
 		Name = "Lucky Kingdom",
 		Text = "<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Lucky Kingdom!</font>"
 	},
-	7 = {
+	"<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Nipon City!</font>" = {
 		Name = "Nipon City",
 		Text = "<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Nipon City!</font>"
 	},
-	8 = {
+	"<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Mori Town!</font>" = {
 		Name = "Mori Town",
 		Text = "<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Mori Town!</font>"
 	}
