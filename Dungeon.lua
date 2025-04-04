4c4f895d5dcd292886d424709d77356c6f26bd50
KamiKaze-rblx <rblxkamikaze@gmail.com>
2025-04-04 17:30:35 +0300

    Update Dungeon.lua

--- Dungeon.lua
+++ Dungeon.lua
@@ -1,7 +1,8 @@
 local v0 = {
 	Solo = {
 		Name = "Leveling City",
-		Text = string.format("<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Leveling City!</font>")
+		Text = '<font color="#00ffff" weight="heavy">[DUNGEON] DUNGEON SPAWNED IN Leveling City!</font>'
 	}
 };
+
 return v0;
